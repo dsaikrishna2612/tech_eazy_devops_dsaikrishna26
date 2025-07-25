12faeff323a5a00cb93e9c3663b0572490fb0869
krishna <dsaikrishna784@gmail.com>
2025-07-25 17:06:51 +0530

    assignment-2

---
+++ assignment-2/variables.tf
@@ -0,0 +1,16 @@
+variable "bucket_name" {
+  description = "dsk2646565"
+  type        = string
+}
+
+variable "instance_ami" {
+  default = "ami-0b6acaa45fec15278"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  default = "intern"
+}
